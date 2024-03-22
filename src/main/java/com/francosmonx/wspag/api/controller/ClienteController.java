df40af314538c2921f513387291538dd21ecc324
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-03-21 22:42:20 -0300

    criação de modelos do dominio da aplicação - criação do modelo de cliente

--- src/main/java/com/francosmonx/wspag/api/controller/ClienteController.java
+++ src/main/java/com/francosmonx/wspag/api/controller/ClienteController.java
@@ -3,12 +3,18 @@ package com.francosmonx.wspag.api.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//responsável por receber requisições HTTP
+import com.francosmonx.wspag.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
-	@GetMapping("/cliente")//uri
-	public String cliente() {
-		return "Juliano";
+	@GetMapping("/cliente")
+	public Cliente cliente() {
+		var cli = new Cliente();
+
+		cli.setNome("Juliano");
+		cli.setId(1);
+		cli.setEmail("juliano.dev@hotmail.com");
+		cli.setTelefone("000900000000");
+		return cli;
 	}
 }
