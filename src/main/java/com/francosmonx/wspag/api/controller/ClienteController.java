6b5234ea7c3a88479b4b0c886dbed33a746104fe
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-03-23 20:59:59 -0300

    fazendo o mapeamento do objeto ao invés do metodo - diminuir o codigo

--- src/main/java/com/francosmonx/wspag/api/controller/ClienteController.java
+++ src/main/java/com/francosmonx/wspag/api/controller/ClienteController.java
@@ -6,12 +6,14 @@ import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.francosmonx.wspag.domain.model.Cliente;
 import com.francosmonx.wspag.domain.repository.ClienteRepository;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 	
 	private ClienteRepository clienteRepository;
@@ -20,12 +22,11 @@ public class ClienteController {
 		this.clienteRepository = repositorio;
 	}
 	
-	@GetMapping("/clientes")
 	public List<Cliente> clientes() {
 		return clienteRepository.findAll();
 	}
 	
-	@GetMapping("/clientes/{clienteId}")
+	@GetMapping("/{clienteId}")
 	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
 		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
 		
