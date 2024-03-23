f8c866178a9c72a0037acd1f1d69841bb263332b
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-03-23 19:35:35 -0300

    fazendo a primeira busca com jakarta diretamente do BD

--- src/main/java/com/francosmonx/wspag/api/controller/ClienteController.java
+++ src/main/java/com/francosmonx/wspag/api/controller/ClienteController.java
@@ -1,29 +1,24 @@
 package com.francosmonx.wspag.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.francosmonx.wspag.domain.model.Cliente;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
 @RestController
 public class ClienteController {
+	//usado para fazer operações com o BD, de modo resumido.
+	@PersistenceContext//o proprio framework injeta uma instancia. Não precisa criar uma nova
+	private EntityManager manager;
 	
 	@GetMapping("/clientes")
 	public List<Cliente> clientes() {
-		var cli = new Cliente();
-
-		cli.setNome("Juliano Henrique");
-		cli.setId(1);
-		cli.setEmail("juliano.dev@hotmail.com");
-		cli.setTelefone("000900000000");
-		
-		var cli1 = new Cliente();
-		cli1.setNome("eduardo");
-		cli1.setId(2);
-		cli1.setEmail("eduardo.dev@hotmail.com");
-		cli1.setTelefone("000900000001");
-		return Arrays.asList(cli,cli1);//criação de listas resumida
+		List<Cliente> clientes = manager.createQuery("from Cliente", Cliente.class).getResultList();
+		return clientes;//busca informando o tipo que é para ser retornado
 	}
 }
--- src/main/java/com/francosmonx/wspag/domain/model/Cliente.java
+++ src/main/java/com/francosmonx/wspag/domain/model/Cliente.java
@@ -1,9 +1,25 @@
 package com.francosmonx.wspag.domain.model;
 
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Cliente {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	
+	@Column //quando não é um ID, ele ira entender como column
 	private String email;
+	@Column
 	private String nome;
+	
+	@Column(name = "fone")
 	private String telefone;
 	
 	public long getId() {
@@ -29,6 +45,21 @@ public class Cliente {
 	}
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return id == other.id;
 	}
 	
 	
