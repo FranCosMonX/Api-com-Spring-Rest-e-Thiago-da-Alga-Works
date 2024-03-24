e6d20f56656607ab45f3c559e7cc3babc9049748
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-03-23 22:18:26 -0300

    adicionando o UPDATE do CRUD - usando o verbo PUT

--- src/main/java/com/francosmonx/wspag/api/controller/ClienteController.java
+++ src/main/java/com/francosmonx/wspag/api/controller/ClienteController.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -45,5 +47,15 @@ public class ClienteController {
 	@PostMapping
 	public Cliente adicionar(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
+	}
+	
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, @RequestBody Cliente clienteAtualizado) {
+		if(!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		clienteAtualizado.setId(clienteId);//para evitar o problema mencionado logo abaixo
+		return ResponseEntity.ok(clienteRepository.save(clienteAtualizado));//só isto faria com que fosse sempre adicionado. pois essa funcionalidade add quando não tem
 	}
 }
