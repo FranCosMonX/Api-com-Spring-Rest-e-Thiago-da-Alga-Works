7ad04628ff27d8b6b65f7f6785a877cce2fbd918
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-03-23 23:47:03 -0300

    levantando a exceção apenas quando o cliente tiver sendo cadastrado

--- src/main/java/com/francosmonx/wspag/domain/service/CadastroClienteService.java
+++ src/main/java/com/francosmonx/wspag/domain/service/CadastroClienteService.java
@@ -1,5 +1,7 @@
 package com.francosmonx.wspag.domain.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,7 +19,11 @@ public class CadastroClienteService {
 	
 	@Transactional
 	public Cliente salvar(Cliente cliente) {
-		if(clienteRepository.findByEmail(cliente.getEmail()).isPresent()) {
+		//há um problema no momento da edição, ja que estou usando esta funcionalidade para atualizar dados do cliente alem de o cadastrar
+		//para corrigir, irei filtrar, do que me é retornado, APENAS os clientes que não estao "se editando"
+		if(clienteRepository.findByEmail(cliente.getEmail())
+				.filter(c -> !c.equals(cliente))
+				.isPresent()) {
 			throw new NegocioException("Email ja em uso");
 		}
 		
