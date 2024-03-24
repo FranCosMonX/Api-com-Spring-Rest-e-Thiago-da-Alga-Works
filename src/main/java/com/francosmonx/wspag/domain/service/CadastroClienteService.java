f0698cc03c6438f3af0553a3e08ec843248e4f65
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-03-23 23:36:35 -0300

    adicionando a regra de negocio que evita que um usuário use no cadastro o email que já está sendo usado por outro usuario ja cadastrado no sistema

--- src/main/java/com/francosmonx/wspag/domain/service/CadastroClienteService.java
+++ src/main/java/com/francosmonx/wspag/domain/service/CadastroClienteService.java
@@ -3,15 +3,9 @@ package com.francosmonx.wspag.domain.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.francosmonx.wspag.domain.exception.NegocioException;
 import com.francosmonx.wspag.domain.model.Cliente;
 import com.francosmonx.wspag.domain.repository.ClienteRepository;
-
-/**
- * - neste pacote contem as regras de negocios
- * - Muito usado principalmente quando precisa trabalhar com varias entidades ou injetar objetos, como um repositorio.
- *
- * - "se tiver uma regra de negocio, não precisará mexer na classe do controlador"
- */
 
 @Service
 public class CadastroClienteService {
@@ -21,9 +15,12 @@ public class CadastroClienteService {
 		this.clienteRepository = clienteR;
 	}
 	
-	//para tirar a função de cadastro do controlador
-	@Transactional //do spring.framework - ou realiza tudo ou dá o desfaz tudo o que foi feito para manter a integridade
-	public Cliente salvar(Cliente cliente) {//essemetodo deve ser executado dentro de uma transação
+	@Transactional
+	public Cliente salvar(Cliente cliente) {
+		if(clienteRepository.findByEmail(cliente.getEmail()).isPresent()) {
+			throw new NegocioException("Email ja em uso");
+		}
+		
 		return clienteRepository.save(cliente);
 	}
 	
