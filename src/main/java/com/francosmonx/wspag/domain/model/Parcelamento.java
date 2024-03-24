ba85125da2ea7f5c2ba8e8f6887830e3f60955a3
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-03-24 19:24:58 -0300

    fazendo validações com o cliente mencionado em um boleto

--- src/main/java/com/francosmonx/wspag/domain/model/Parcelamento.java
+++ src/main/java/com/francosmonx/wspag/domain/model/Parcelamento.java
@@ -10,6 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -21,9 +22,10 @@ public class Parcelamento {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Valid //fazendo a validação do objeto para evitar que o id venha null
 	@NotNull
-	@ManyToOne //relacionamento
-	@JoinColumn(name = "cliente_id") //
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
 	
 	@NotBlank
@@ -33,9 +35,8 @@ public class Parcelamento {
 	private BigDecimal valor_total;
 	@NotNull
 	@Positive
-	@Max(value = 12)
+	@Max(12)
 	private Integer quantidade_parcelas;
-	
 	private LocalDateTime dataCriacao;
 	
 	
@@ -63,10 +64,10 @@ public class Parcelamento {
 	public void setValor_total(BigDecimal valor_total) {
 		this.valor_total = valor_total;
 	}
-	public Integer getQuantidade_de_parcelas() {
+	public Integer getQuantidade_parcelas() {
 		return quantidade_parcelas;
 	}
-	public void setQuantidade_de_parcelas(Integer quantidade_de_parcelas) {
+	public void setQuantidade_parcelas(Integer quantidade_de_parcelas) {
 		this.quantidade_parcelas = quantidade_de_parcelas;
 	}
 	public LocalDateTime getDataCriacao() {
