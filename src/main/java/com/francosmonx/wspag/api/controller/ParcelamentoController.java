180c37554b5bfbbff797e5eb15e01dfc1041d6a8
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-03-24 21:27:10 -0300

    alterando informações básicas das mensagens de erros

--- src/main/java/com/francosmonx/wspag/api/controller/ParcelamentoController.java
+++ src/main/java/com/francosmonx/wspag/api/controller/ParcelamentoController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.francosmonx.wspag.domain.exception.NegocioException;
 import com.francosmonx.wspag.domain.model.Parcelamento;
 import com.francosmonx.wspag.domain.repository.ParcelamentoRepository;
 import com.francosmonx.wspag.domain.service.ParcelamentoService;
--- src/main/java/com/francosmonx/wspag/api/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/francosmonx/wspag/api/exceptionHandler/ApiExceptionHandler.java
@@ -1,19 +1,34 @@
 package com.francosmonx.wspag.api.exceptionHandler;
 
+import java.net.URI;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.francosmonx.wspag.domain.exception.NegocioException;
 
-/**
- * componente spring que é capaz de capturar todas as exceções da aplicação
- */
-
 @RestControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 
+	//costumizando informações
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		
+		ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+		problemDetail.setTitle("Um ou mais campos estao invalidos.");
+		problemDetail.setType(URI.create("https://francosmonx.com/erros/campos-invalidos"));
+		
+		return super.handleExceptionInternal(ex, problemDetail, headers, status, request);
+	}
+	
 	@ExceptionHandler(NegocioException.class)
 	public ResponseEntity<String> capturar(NegocioException e) {
 		return ResponseEntity.badRequest().body(e.getMessage());
