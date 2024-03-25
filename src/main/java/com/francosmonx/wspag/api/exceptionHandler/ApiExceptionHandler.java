cb7412c0bcea9f1b5f81e0c398423c5ee99c1529
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-03-24 22:07:49 -0300

    adicionando origens de erro

--- src/main/java/com/francosmonx/wspag/api/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/francosmonx/wspag/api/exceptionHandler/ApiExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.francosmonx.wspag.api.exceptionHandler;
 
 import java.net.URI;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -25,6 +27,14 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 		ProblemDetail problemDetail = ProblemDetail.forStatus(status);
 		problemDetail.setTitle("Um ou mais campos estao invalidos.");
 		problemDetail.setType(URI.create("https://francosmonx.com/erros/campos-invalidos"));
+		
+		var fields = ex.getBindingResult().getAllErrors() //pega todos os objetos de erro da exceção definindo cada campo com algum erro
+			.stream()//usando o stream para navegar pela lista
+			//toMap precisa ter duas funções, para retornar o nome da chave do mapa e outra para o valor do mapa
+			.collect(Collectors.toMap(error -> ((FieldError)error).getField(), 
+					error -> error.getDefaultMessage()));
+		
+		problemDetail.setProperty("Fields", fields);
 		
 		return super.handleExceptionInternal(ex, problemDetail, headers, status, request);
 	}
